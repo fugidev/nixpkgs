0b117b133530c8112ab0a6d4adf97affbdf6be5a
Fugi <me@fugi.dev>
2025-06-05 15:05:40 +0200

    working x86_64-linux build, broken executable

--- pkgs/by-name/jo/joplin-desktop/package.nix
+++ pkgs/by-name/jo/joplin-desktop/package.nix
@@ -1,14 +1,19 @@
 {
+  lib,
   stdenv,
   nodejs,
+  makeDesktopItem,
+  copyDesktopItems,
+  makeWrapper,
   fetchFromGitHub,
   yarn-berry_3,
   python3,
   pkg-config,
-  # sqlite,
   pango,
   cairo,
   pixman,
+  libsecret,
+  electron,
 }:
 
 let
@@ -26,20 +31,6 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-DDHUBtIjzeFL/iMaHDniOSLS3Y8Mwcp6BuHc+w/u8Ic=";
   };
 
-  nativeBuildInputs = [
-    nodejs
-    yarn-berry.yarnBerryConfigHook
-    (python3.withPackages(ps: with ps; [ distutils ]))
-    pkg-config
-    pango
-    cairo
-    pixman
-  ];
-
-  buildInputs = [
-    # sqlite
-  ];
-
   missingHashes = ./missing-hashes.json;
 
   offlineCache = yarn-berry.fetchYarnBerryDeps {
@@ -47,16 +38,49 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-obMlkN2Ajq7UetAPTdRjzT/BkJSYQP74uP35dowWprA=";
   };
 
+  nativeBuildInputs = [
+    nodejs
+    yarn-berry.yarn-berry-offline
+    yarn-berry.yarnBerryConfigHook
+    (python3.withPackages(ps: with ps; [ distutils ]))
+    pkg-config
+    pango
+    cairo
+    pixman
+    libsecret
+    makeWrapper
+  ]
+  # ++ lib.optionals stdenv.hostPlatform.isDarwin [ xcbuild ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [ copyDesktopItems ];
+
+  env = {
+    BUILD_SEQUENCIAL = 1;
+    YARN_ENABLE_SCRIPTS = 0;
+    ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
+  };
+
   postPatch = ''
-    sed -i '/postinstall/d' package.json
+    sed -i "/'buildDefaultPlugins',/d" packages/app-desktop/gulpfile.ts
   '';
 
   buildPhase = ''
     runHook preBuild
 
+    yarn workspaces foreach \
+      --verbose \
+      --interlaced \
+      --topological \
+      --recursive \
+      --from @joplin/app-desktop \
+      run build
+    yarn tsc
+
     cd packages/app-desktop
     mkdir dist && touch dist/AppImage
-    yarn run dist --dir # --config.asar=false
+    yarn run electron-builder \
+      --dir \
+      -c.electronDist="${electron.dist}" \
+      -c.electronVersion=${electron.version}
 
     runHook postBuild
   '';
@@ -64,9 +88,38 @@ stdenv.mkDerivation (finalAttrs: {
   installPhase = ''
     runHook preInstall
 
-    mkdir -p "$out/share/joplin-desktop"
-    cp -r dist/*unpacked "$out/share/joplin-desktop"
+    ${lib.optionalString stdenv.hostPlatform.isLinux ''
+      mkdir -p "$out/share/joplin-desktop"
+      cp -r dist/*unpacked/* "$out/share/joplin-desktop"
+
+      for file in "$src/Assets/LinuxIcons"/*.png; do
+        resolution=$(basename "$file" .png)
+        install -Dm644 "$file" "$out/share/icons/hicolor/$resolution/apps/joplin.png"
+      done
+
+      # makeWrapper $out/share/joplin-desktop/joplin $out/bin/joplin-desktop \
+      #   --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations --enable-wayland-ime}}" \
+      #   --inherit-argv0
+
+      makeWrapper ${lib.getExe electron} $out/bin/joplin-desktop \
+        --add-flags $out/share/joplin-desktop/resources/app.asar \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations --enable-wayland-ime}}" \
+        --inherit-argv0
+    ''}
 
     runHook postInstall
   '';
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "joplin";
+      desktopName = "Joplin";
+      exec = "joplin-desktop --no-sandbox %U";
+      icon = "joplin";
+      comment = "Joplin for Desktop";
+      categories = [ "Office" ];
+      startupWMClass = "@joplin/app-desktop";
+      mimeTypes = [ "x-scheme-handler/joplin" ];
+    })
+  ];
 })
